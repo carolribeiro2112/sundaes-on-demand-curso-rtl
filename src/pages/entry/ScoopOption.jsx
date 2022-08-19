2e86e2a7acaa43b160fa32d68a5072a7d06b75b9
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-08-19 20:06:00 -0300

    feature: code Optiond and ScoopOption components

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,5 +1,15 @@
-function ScoopOption() {
-  return <div />;
+import Col from "react-bootstrap/Col";
+
+function ScoopOption({ name, imagePath }) {
+  return (
+    <Col sx={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} scoop`}
+      />
+    </Col>
+  );
 }
 
 export default ScoopOption;
