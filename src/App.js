bbf5f0ac4916c96bd9819fe9137a9c9ebe29b5f1
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-08-21 20:28:31 -0300

    feature: Adding new things to ScoopOptions component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import "./App.css";
-import SummaryForm from "./pages/summary/SummaryForm";
+import Container from "react-bootstrap/Container";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+import OrderEntry from "./pages/entry/OrderEntry";
 
 function App() {
   return (
-    <div className="App">
-      <SummaryForm />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+      </OrderDetailsProvider>
+    </Container>
   );
 }
 
