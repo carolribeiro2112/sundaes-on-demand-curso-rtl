e8357528c4cf11ca23cce5d594b0f93b7f5e4516
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-08-22 11:47:44 -0300

    feature: Create test-utils

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,11 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 
 import Options from "../Options";
 
 describe("Options component", () => {
   it("displays image for each scoop option from server", async () => {
-    render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+    render(<Options optionType="scoops" />);
 
     //find the images
     const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
@@ -20,7 +19,7 @@ describe("Options component", () => {
     render(<Options optionType="toppings" />);
 
     const toppingsImages = await screen.findAllByRole("img", {
-      name: /topping $/i,
+      name: /topping$/i,
     });
     expect(toppingsImages).toHaveLength(3);
 
