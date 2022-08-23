b61a8f77ea5f313bf807e71416520c30f76431a7
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-08-23 18:34:24 -0300

    feature: add checkboxes to the toppingsOptions component and write tests for it

--- src/pages/entry/test/TotalUpdates.test.jsx
+++ src/pages/entry/test/TotalUpdates.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 import Options from "../Options";
@@ -32,5 +32,34 @@ describe("TotalUpdates", () => {
     userEvent.clear(chocolateInput);
     userEvent.type(chocolateInput, "2");
     expect(scoopsSubtotal).toHaveTextContent("6.00");
+  });
+
+  it("Should update toppings subtotal when toppings change", async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingsTotal = screen.getByText("Toppings total: $", {
+      exact: false,
+    });
+    expect(toppingsTotal).toHaveTextContent("0.00");
+
+    const cherriesCheckbox = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+    userEvent.click(cherriesCheckbox);
+    expect(toppingsTotal).toHaveTextContent("1.50");
+
+    const hotFudgeCheckbox = screen.getByRole("checkbox", {
+      name: "Hot fudge",
+    });
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent("3.00");
+
+    const MeMsCheckbox = screen.getByRole("checkbox", { name: "M&Ms" });
+    userEvent.click(MeMsCheckbox);
+    expect(toppingsTotal).toHaveTextContent("4.50");
+
+    //remove one topping
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent("3.00");
   });
 });
