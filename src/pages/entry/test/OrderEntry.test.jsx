bbbc31526b47126efbbdd76fd3ee6c4db74e083d
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-09-13 19:28:38 -0300

    feature: add setOrderPhase mock to OrderEntry test

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -18,7 +18,7 @@ describe("OrderEntry component", () => {
       )
     );
 
-    render(<OrderEntry />);
+    render(<OrderEntry setOrderPhase={jest.fn()} />);
 
     await waitFor(async () => {
       const alerts = await screen.findAllByRole("alert");
